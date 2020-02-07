ec8467d86ab631759d18735a786f9662cd291d99
SenadDelic <dellic.senad@gmail.com>
2020-02-07 19:52:24 +0100

    fixed Employee

--- src/person/Employe.java
+++ src/person/Employe.java
@@ -4,43 +4,39 @@ import java.util.Date;
 
 import hospital.Salary;
 
-public abstract class Employe extends Person{
-	private Salary salary;
-	private Date startDate;
-	private Department department;
-	
-	
-	
-	public Employe() {
-		super();
-	}
-	
-	
-	public Employe(String firstName, String lastName, Date dateOfBirth) {
-		super(firstName, lastName, dateOfBirth);
-		
-	}
+public abstract class Employe extends Person {
+    private Salary salary;
+    private Date startDate;
+    private Department department;
+
+    public Employe() {
+        super();
+    }
+
+    public Employe(String firstName, String lastName, Date dateOfBirth, String userName, String password) {
+        super(firstName, lastName, dateOfBirth, userName, password);
+    }
+
+    public Salary getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Salary salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 
 
-	public Salary getSalary() {
-		return salary;
-	}
-	public void setSalary(Salary salary) {
-		this.salary = salary;
-	}
-	public Department getDepartment() {
-		return department;
-	}
-	public void setDepartment(Department department) {
-		this.department = department;
-	}
-
-
-	@Override
-	public String toString() {
-		return "Employe [startDate=" + startDate + "]";
-	}
+    @Override
+    public String toString() {
+        return "Employe [startDate=" + startDate + "]";
+    }
 	
-	
-
 }
