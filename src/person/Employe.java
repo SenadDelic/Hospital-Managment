cadef4234d9a968a05836fc5b1b94ede971d531a
Hata Mujic <hata.mujic@gmail.com>
2020-02-07 19:23:53 +0100

    first commit

---
+++ src/person/Employe.java
@@ -0,0 +1,46 @@
+package person;
+
+import java.util.Date;
+
+import hospital.Salary;
+
+public abstract class Employe extends Person{
+	private Salary salary;
+	private Date startDate;
+	private Department department;
+	
+	
+	
+	public Employe() {
+		super();
+	}
+	
+	
+	public Employe(String firstName, String lastName, Date dateOfBirth) {
+		super(firstName, lastName, dateOfBirth);
+		
+	}
+
+
+	public Salary getSalary() {
+		return salary;
+	}
+	public void setSalary(Salary salary) {
+		this.salary = salary;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Employe [startDate=" + startDate + "]";
+	}
+	
+	
+
+}
